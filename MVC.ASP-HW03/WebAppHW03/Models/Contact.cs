c3a2393f30a199f076ce1d1820095b405b1eca30
Gjorgji Kongulovski <46351282+Hellinho85@users.noreply.github.com>
2019-06-17 20:43:59 +0200

    MVC.ASP-HW03
    MVC.ASP-HW03 - Phonebook

---
+++ MVC.ASP-HW03/WebAppHW03/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppHW03.Models
+{
+    // Model class with required properties and information;
+    public class Contact
+    {
+        // FirstName property with required (min and max length) validation form;
+        [Required]
+        [MinLength(3), MaxLength(25)]
+        public string FirstName { get; set; }
+
+        // LastName property with required (min and max length) validation form;
+        [Required]
+        [MinLength(3), MaxLength(25)]
+        public string LastName { get; set; }
+
+        // IsCloseFriend property with required validation;
+        [Required]
+        public bool IsCloseFriend { get; set; }
+
+        // PhoneNumber property with required (range digits) validation form;
+        [Required]
+        [Range(10000000, 99999999999)]
+        public long PhoneNumber { get; set; }
+    }
+}
---
+++ MVC.ASP-HW03/WebAppHW03/Models/ListOfContacts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppHW03.Models
+{
+    public class ListOfContacts
+    {
+        // Model class with ListOfContacts property;
+        public List<Contact> MyContacts { get; set; }
+    }
+}
